465f695167aa46440b8e68ce70bc8800d6ac7389
Matvey <matveyprotasenya@gmail.com>
2023-05-04 19:02:09 +0300

    final+plus

--- 1829/Program.cs
+++ 1829/Program.cs
@@ -10,49 +10,7 @@ namespace _1829
     {
         static void Main(string[] args)
         {
-            int exit = -1;
-            Console.WriteLine("Введите число A:");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите число B:");
-            int b = Convert.ToInt32(Console.ReadLine());
-            while (exit != 0)
-            {
-                Console.WriteLine("1 + ) 2 - ) 3 * ) 4 / )");
-                exit = Convert.ToInt32(Console.ReadLine());
-                switch (exit)
-                {
-                    case 0:
-                        Console.WriteLine("Программа завершена.");
-                        break;
-                    case 1:
-                        {
-                            int result = a + b;
-                            Console.WriteLine("Результат: "+result);
-                            break;
-                        }
-                    case 2:
-                        {
-                            int result = a - b;
-                            Console.WriteLine("Результат: " + result);
-                            break;
-                        }
-                    case 3:
-                        {
-                            int result = a * b;
-                            Console.WriteLine("Результат: " + result);
-                            break;
-                        }
-                    case 4:
-                        {
-                            double result = a / b;
-                            Console.WriteLine("Результат: " + result);
-                            break;
-                        }
-                    default:
-                        Console.WriteLine("Введите верный элемент меню");
-                        break;
-                }
-            }
+            
         }
     }
 }
